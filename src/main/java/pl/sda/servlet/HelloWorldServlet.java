73f94cfc9d314f9d05e7183612673f19010ffabc
Robert Pilczuk <robertpilczuk@gmail.com>
2020-08-23 11:24:15 +0200

    Zad22

--- src/main/java/pl/sda/servlet/HelloWorldServlet.java
+++ src/main/java/pl/sda/servlet/HelloWorldServlet.java
@@ -7,14 +7,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet",
-urlPatterns = "/")
+//@WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter writer = response.getWriter();
-        writer.print("Hello world");
+        writer.println("UserId: " + getInitParameter("userId"));
+        writer.println("Localization: " + getInitParameter("localization"));
 
     }
+
+    //jakbyśmy chcieli identycznie obsługiwać post i get to:
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        doGet(request, response);
+    }
+
 }
