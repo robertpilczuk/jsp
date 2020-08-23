47123c50758503590367e7ab7ea642151c0363f1
Robert Pilczuk <robertpilczuk@gmail.com>
2020-08-23 13:07:51 +0200

    Zad22, zad23, zad24, zad25

---
+++ src/main/java/pl/sda/filter/MyFilter.java
@@ -0,0 +1,38 @@
+package pl.sda.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//@WebFilter(filterName = "MyFilter", urlPatterns = "/")
+@WebFilter(filterName = "MyFilter", servletNames = "HelloWorldServlet")
+public class MyFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        PrintWriter writer = servletResponse.getWriter();
+        writer.write(
+                "<html><body><div>"
+        );
+        String login = servletRequest.getParameter("login");
+        if (login != null && login.equals("admin")){
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            writer.write(
+                    "<p style='color: red; font-size: larger'>User id is not valid. Please try again...</p>"
+            );
+        }
+        writer.write("</div></body></html>");
+        writer.close();
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
--- src/main/java/pl/sda/servlet/HelloWorldServlet.java
+++ src/main/java/pl/sda/servlet/HelloWorldServlet.java
@@ -1,27 +1,24 @@
 package pl.sda.servlet;
-
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
-//@WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
+@WebServlet(name = "HelloWorldServlet",
+        urlPatterns = {"/", "/hello"},
+        initParams = {
+                @WebInitParam(name = "localization", value = "Poland"),
+                @WebInitParam(name = "newParam", value = "newValue")
+        },
+        loadOnStartup = 1
+)
 public class HelloWorldServlet extends HttpServlet {
-
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter writer = response.getWriter();
-        writer.println("UserId: " + getInitParameter("userId"));
+        writer.println("newParam: " + getInitParameter("newParam"));
         writer.println("Localization: " + getInitParameter("localization"));
-
     }
-
-    //jakbyśmy chcieli identycznie obsługiwać post i get to:
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        doGet(request, response);
-    }
-
 }
